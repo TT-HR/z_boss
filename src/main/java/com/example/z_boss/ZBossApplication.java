409038c3d5a626fe762819e8bc984ade713839f8
yanghaoran <8251274402qq.com>
2023-07-04 17:17:19 +0800

    初始化提交

---
+++ src/main/java/com/example/z_boss/ZBossApplication.java
@@ -0,0 +1,13 @@
+package com.example.z_boss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZBossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZBossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/z_boss/ZBossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.z_boss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZBossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
